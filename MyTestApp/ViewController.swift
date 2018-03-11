8079973e587425475da9f02780acb43c51fdf6b3
Caesar Wirth <cjwirth@gmail.com>
2018-03-11 12:20:00 -0400

    Initial Commit

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by Caesar Wirth on 3/11/18.
+//  Copyright © 2018 Caesar Wirth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

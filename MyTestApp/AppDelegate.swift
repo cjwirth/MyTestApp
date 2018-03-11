e80e2a1c09d7920cc7983397c80fa595e3ef2d23
Caesar Wirth <cjwirth@gmail.com>
2018-03-11 13:41:51 -0400

    This commit doesn't add any violations

--- MyTestApp/AppDelegate.swift
+++ MyTestApp/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // this commit doesn't add any violations.
         return true
     }
 
@@ -49,7 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        
+
     }
 
 
